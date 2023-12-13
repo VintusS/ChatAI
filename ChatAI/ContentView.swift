8102cdaf91128b2ff1e3616acc1d03e1e221d9f6
Dragomir Mindrescu <121290652+VintusS@users.noreply.github.com>
2023-12-13 15:52:03 +0200

    Token leak fix

--- ChatAI/ContentView.swift
+++ ChatAI/ContentView.swift
@@ -14,7 +14,8 @@ final class ViewModel: ObservableObject {
     private var client: OpenAISwift?
     
     func setup() {
-            let config = OpenAISwift.Config.makeDefaultOpenAI(apiKey: "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")
+            // change the apiKey with your token
+            let config = OpenAISwift.Config.makeDefaultOpenAI(apiKey: "token")
             self.client = OpenAISwift(config: config)
         }
     
